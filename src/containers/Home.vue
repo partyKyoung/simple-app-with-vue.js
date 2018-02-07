fd151ca3265459a18a9fab985cd1b1c0fa8b3ee6
KyoungAh <kyoungah@kyoungah.com>
2018-02-08 01:08:53 +0900

    홈 작성

---
+++ src/containers/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>고독한 김생민 In Web</h1>
+    <h2>돈은 안쓰는 것이다.</h2>
+    <div>
+      <button>입력하기</button>
+    </div>
+    <ul v-for="item of list" :key="item.key">
+      <li>
+        {{item.target}}
+        가격: {{item.price}}원
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      list: [
+        {key: 1, target: '옹아리 인형', status: 'great', price: 30000}, 
+        {key: 2, target: '서피스 4', status: 'great', price: 1560000}, 
+        {key: 3, target: '배고프당', status: 'great', price: 100}, 
+        {key: 4, target: '스타벅스 돌체라떼', status: 'great', price: 6500}
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
